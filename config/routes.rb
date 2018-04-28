bdaa1e29ec4814065bf5e6d481529f4722983a2d
Kasia Kawa <kasiakawa@Kasias-MacBook-Air.local>
2018-04-28 16:07:35 +0200

    Refactor controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  # CRUD create, read, update, delete
   root 'welcome#index'
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new', as: 'new_article'
-  post 'articles', to: 'articles#create'
-  get 'articles/:id', to: 'articles#show', as: 'article'
-  get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
-  patch 'articles/:id', to: 'articles#update'
-  delete 'articles/:id', to: 'articles#destroy'
+  # get 'articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new', as: 'new_article'
+  # post 'articles', to: 'articles#create'
+  # get 'articles/:id', to: 'articles#show', as: 'article'
+  # get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  # patch 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :articles
 end
