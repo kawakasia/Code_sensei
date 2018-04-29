c6e93d2a488f15420453c83f56d46609e717b17f
Kasia Kawa <kapuczino.kate@gmail.com>
2018-04-29 10:23:28 +0200

    Create controller for comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController <ApplicationController
+class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :update, :edit, :destroy]
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # delete 'articles/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :articles
+resources :comments
 end
